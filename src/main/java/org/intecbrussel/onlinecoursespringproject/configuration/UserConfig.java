f8995d2aee62a915c3e8582e598832a044267d46
oifjq <bram.labarque@gmail.com>
2026-01-06 15:33:26 +0100

    start

--- src/main/java/org/intecbrussel/onlinecoursespringproject/configuration/UserConfig.java
+++ src/main/java/org/intecbrussel/onlinecoursespringproject/configuration/UserConfig.java
@@ -4,20 +4,26 @@ import org.intecbrussel.onlinecoursespringproject.model.Role;
 import org.intecbrussel.onlinecoursespringproject.model.User;
 import org.intecbrussel.onlinecoursespringproject.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//@SpringBootApplication = @Configuration, @EnableAutoConfiguration and @ComponentScan
 @Configuration
 public class UserConfig {
 
     @Bean
     CommandLineRunner commandLineRunner(UserRepository userRepository) {
         return String[] args -> {
+            User instr1 = new User();
+            instr1.setUserName("user1");
+            instr1.setEmail("a.b@c.com");
+            instr1.setRole(Role.INSTRUCTOR);
+            instr1.setPasswordHashed("123");
 
-            //String userName, String email, Role role, String passwordHashed
-            User instructor = new User(0, "user1", "a.b@c.com", Role.INSTRUCTOR);
-            userRepository.save(instructor);
-        }
+            //User instructor = new User(0, "user1", "a.b@c.com", Role.INSTRUCTOR);
+            //userRepository.save(instr1);
+        };
     }
 
 }
